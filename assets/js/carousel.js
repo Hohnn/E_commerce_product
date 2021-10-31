764d8dc5e94fa30c99f71ad9c05585dfda4ba84c
Hohnn <jean-bateee@hotmail.fr>
2021-10-31 15:18:07 +0100

    finish carousel js

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -8,4 +8,92 @@ function orderCarouselImages() {
     });
 }
 
-orderCarouselImages();
+/* orderCarouselImages(); */
+
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+const allImages = document.querySelectorAll('[data-carousel]');
+const allImagesPreview = document.querySelectorAll('[data-preview]');
+
+function carouselArrow (direction){
+    let active = document.querySelector('.carousel .active')
+    let activeData = active.getAttribute('data-carousel');
+    active.classList.add('fading');
+    active.classList.remove('active');
+    let prevImageCount = (direction == 'prev') ? activeData - 1 : +activeData + 1;
+    let prevImage = allImages[prevImageCount - 1]
+    if (prevImageCount == 0){
+        prevImage = allImages[allImages.length - 1]
+    } else if (prevImageCount > allImages.length){
+        prevImage = allImages[allImages.length - (allImages.length)]
+    }
+    prevImage.classList.add('active')
+    setTimeout(()=>{
+        active.classList.remove('fading');
+    }, 300)
+    activePreview(prevImage)
+}
+
+prevBtn.addEventListener('click', () => { 
+    carouselArrow('prev')
+})
+nextBtn.addEventListener('click', () => { 
+    carouselArrow('next')
+})
+
+const preview = document.getElementById('preview')
+
+window.addEventListener('keydown', (event) => {
+    if (event.code === 'ArrowRight') {
+        carouselArrow('next')
+    } else if (event.code === 'ArrowLeft') {
+        carouselArrow('prev')
+    }
+});
+
+
+function activePreview (param) {
+    let activeData = param.getAttribute('data-carousel')
+    let target = allImagesPreview[activeData - 1]
+    let active = document.querySelector('.itemPreview .active');
+    active.classList.remove('active')
+    target.classList.add('active')
+}
+
+allImagesPreview.forEach( element => {
+    element.addEventListener('click', function () {
+        let data = this.dataset.preview
+        let active = document.querySelector('.carousel .active')
+        active.classList.add('fading');
+        active.classList.remove('active');
+        let target = allImages[data - 1]
+        target.classList.add('active')
+        setTimeout(()=>{
+            active.classList.remove('fading');
+        }, 300)
+        activePreview(target)
+    })
+})
+
+const closePreview = document.getElementById('closePreview')
+closePreview.addEventListener('click', () => {
+    preview.classList.remove('open')
+})
+
+
+// main page
+
+const mainImage = document.getElementById('mainImage')
+
+mainImage.addEventListener('click', () => {
+    preview.classList.add('open')
+})
+
+const allThumbnail = document.querySelectorAll('[data-thumbnail]')
+console.log(allThumbnail);
+allThumbnail.forEach( element => {
+    element.addEventListener('click', function (){
+        let source = this.dataset.thumbnail
+        mainImage.querySelector('img').src = `./assets/images/image-product-${source}.jpg`
+    })
+})
